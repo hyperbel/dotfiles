e46b1698f337bd8e93f000812c464bd0698eb127
Hyperbel <25338593+hyperbel@users.noreply.github.com>
2023-02-16 19:00:36 +0100

    telescope integration

--- init.lua
+++ init.lua
@@ -1,3 +1,4 @@
+-- packer
 local use = require('packer').use
 require('packer').startup(function()
 	use 'wbthomason/packer.nvim'
@@ -6,20 +7,26 @@ require('packer').startup(function()
 	use 'nvim-lua/lsp-status.nvim'
 	use 'nvim-lua/lsp_extensions.nvim'
 	use 'github/copilot.vim'
-	use 'nvim-treesitter/nvim-treesitter'
-	use 'nvim-treesitter/playground'
-	use 'nvim-treesitter/nvim-treesitter-context'
-	use 'nvim-telescope/telescope.nvim'
-	use 'nvim-telescope/telescope-fzy-native.nvim'
-	use 'nvim-telescope/telescope-frecency.nvim'
-	use 'nvim-telescope/telescope-media-files.nvim'
-	use 'nvim-telescope/telescope-project.nvim'
-	use 'nvim-telescope/telescope-symbols.nvim'
 	use 'LnL7/vim-nix'
+	use 'kkvh/vim-docker-tools'
+	use 'neovim/nvim-lspconfig'
+	use 'williamboman/nvim-lsp-installer'
+	use 'nvim-lua/plenary.nvim'
+	use 'nvim-lua/telescope.nvim'
 end)
 
 -- vim options from init.vim
 vim.opt.termguicolors = true
 vim.opt.relativenumber = true
+vim.opt.number = true
 vim.cmd("colorscheme slate")
 vim.cmd("syntax on")
+
+-- telescope
+local builtin = require('telescope.builtin')
+--vim.keymap.set('n', ' ', builtin.find_files {})
+vim.cmd("nnoremap <Space> <cmd>Telescope find_files<CR>")
+
+-- lsp
+require'nvim-lsp-installer'.setup{}
+require'lspconfig'.pyright.setup{}
